4ff45ad58b3e17417fdd64a1299ce444ad0681bc
KoknaevD <89024464050@yandex.ru>
2020-08-16 16:17:29 +0500

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,24 @@
 import java.util.*
+val scanner = Scanner(System.`in`)
 
 fun main() {
-    println("Hello world 2")
+    println("Write how many cups of coffee you will need:")
+    val cupsCount = scanner.nextInt()
+    CoffeeCup.printIngredients(cupsCount)
+
+}
+
+
+
+object CoffeeCup {
+    private const val water = 200
+    private const val milk = 50
+    private const val coffeeBeans = 15
+
+    fun printIngredients(count: Int) {
+        println("For $count cups of coffee you will need:")
+        println("${count * water} ml of water")
+        println("${count * milk} ml of milk")
+        println("${count * coffeeBeans} g of coffee beans")
+    }
 }
